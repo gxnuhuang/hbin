69f0c174252eefdcd49a46363e479fb43f1193df
gxnuhuang <343862618@qq.com>
2014-08-23 11:27:28 +0800

    修改系统备份脚本

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ complete -W 'distclean mrproper clean am335x_evm_defconfig menuconfig uImage mod
 hmake与hbuild_irtk支持是否在out目录编译
 
 添加hchange改变档案的拥有者与群组属性命令。complete -W 'gxnuhuang root ' hchange加入到/home/gxnuhuang/.bashrc中
+修改hback.sh备份系统的脚本，第一次备份的时间是'2014-06-29'，这次修改添加对比-N '2014-06-29'时间，只备份比-N '2014-06-29'时间更新的文件。（插入移动硬盘，直接运行）。此次备份时间2014-08-23.
--- hback.sh
+++ hback.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+#COMPARE_TIME=2014-06-29
+echo -e "\033[41;20m--------------------start backup system----------------------\033[0m"
 
-tar -czpv -f /media/gxnuhuang/Data_backup/usr_back_$(date +"%Y-%m-%d").tar.gz /usr | tee /media/gxnuhuang/Data_backup/usr_back_$(date +"%Y-%m-%d").log 
-echo "-----------------------------done usr and start backup for etc-----------------------------------------------"
-tar -czpv -f /media/gxnuhuang/Data_backup/etc_back_$(date +"%Y-%m-%d").tar.gz /etc | tee /media/gxnuhuang/Data_backup/etc_back_$(date +"%Y-%m-%d").log
-echo "-----------------------------done etc and start backup for root-----------------------------------------------"
-tar -czpv -f /media/gxnuhuang/Data_backup/root_back_$(date +"%Y-%m-%d").tar.gz /root | tee /media/gxnuhuang/Data_backup/root_back_$(date +"%Y-%m-%d").log
+tar -N '2014-06-29' -czpv -f /media/gxnuhuang/Data_backup/usr_back_$(date +"%Y-%m-%d").tar.gz /usr | tee /media/gxnuhuang/Data_backup/usr_back_$(date +"%Y-%m-%d").log 
 
-echo "-----------------------------done root and start backup for home-----------------------------------------------"
-tar -czpv -f /media/gxnuhuang/Data_backup/home_sda7_back_$(date +"%Y-%m-%d").tar.gz /home/gxnuhuang/ --exclude=/home/gxnuhuang/share_data/* | tee /media/gxnuhuang/Data_backup/home_back_$(date +"%Y-%m-%d").log
+echo -e "\033[41;20m--------------------done usr and start backup for etc----------------------\033[0m"
 
-echo "-----------------------------done home and start backup for data_h-----------------------------------------------"
-tar -czpv -f /media/gxnuhuang/Data_backup/data_h_sda5_back_$(date +"%Y-%m-%d").tar.gz /data_h  | tee /media/gxnuhuang/Data_backup/data_back_$(date +"%Y-%m-%d").log
+tar -N '2014-06-29' -czpv -f /media/gxnuhuang/Data_backup/etc_back_$(date +"%Y-%m-%d").tar.gz /etc | tee /media/gxnuhuang/Data_backup/etc_back_$(date +"%Y-%m-%d").log
+
+echo -e "\033[41;20m------------------done etc and start backup for root---------------------------\033[0m"
+
+tar -N '2014-06-29' -czpv -f /media/gxnuhuang/Data_backup/root_back_$(date +"%Y-%m-%d").tar.gz /root | tee /media/gxnuhuang/Data_backup/root_back_$(date +"%Y-%m-%d").log
+
+echo -e "\033[41;20m----------------done root and start backup for home--------------------------\033[0m"
+
+tar -N '2014-06-29' -czpv -f /media/gxnuhuang/Data_backup/home_sda7_back_$(date +"%Y-%m-%d").tar.gz /home/gxnuhuang/ --exclude=/home/gxnuhuang/share_data/* | tee /media/gxnuhuang/Data_backup/home_back_$(date +"%Y-%m-%d").log
+
+echo -e "\033[41;20m--------------done home and start backup for data_h---------------------------\033[0m"
+
+tar -N '2014-06-29' -czpv -f /media/gxnuhuang/Data_backup/data_h_sda5_back_$(date +"%Y-%m-%d").tar.gz /data_h  | tee /media/gxnuhuang/Data_backup/data_back_$(date +"%Y-%m-%d").log
